ae1b41594109f2b19f10e18ac39f82b9b6786c93
Anthony Hoang <ant.hoang93@gmail.com>
2015-10-13 09:22:09 -0700

    Implement User model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,54 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  username        :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ActiveRecord::Base
+  attr_reader :password
+  afer_initialize :ensure_session_token
+
+  validates
+    :username,
+    :email,
+    :password_digest,
+    :session_token,
+    presence: true
+
+  validates
+    :username,
+    :email,
+    uniqueness: true
+
+  validates
+    :password,
+    length: {minimum: 6, allow_nil: true}
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user && user.valid_password?(password)
+    user
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    return Bcrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
 end
