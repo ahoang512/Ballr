3f504d8c8bb6d3b8ce328021fd046496cec15be3
Anthony Hoang <ant.hoang93@gmail.com>
2015-10-15 08:12:53 -0700

    Created a folder for edit components

--- app/assets/javascripts/components/edit_page/edit_album.js.jsx
+++ app/assets/javascripts/components/edit_page/edit_album.js.jsx
@@ -2,13 +2,16 @@ var EditAlbum = React.createClass({
   getInitialState : function () {
     return ({albumSelected : 0});
   },
+
   _updateSelected : function(albumId) {
     this.setState({albumSelected : albumId});
   },
+
   render : function () {
+    var username = window.current_username + "'s";
     return (
       <div className="editAlbum">
-        <span className="editLabel">Edit Your Albums</span>
+        <span className="editLabel">{username} Library</span>
         <div className="editContainer group">
           <AlbumTileContainer updateSelected={this._updateSelected}/>
           <PhotoTileContainer/>
