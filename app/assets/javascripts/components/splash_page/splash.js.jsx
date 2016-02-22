816e6971553f806c92f774c69918a70101dcb01a
Anthony Hoang <ant.hoang93@gmail.com>
2016-02-21 20:06:08 -0800

    github and linkedin logos at footer redirect to appropiate addresses

--- app/assets/javascripts/components/splash_page/splash.js.jsx
+++ app/assets/javascripts/components/splash_page/splash.js.jsx
@@ -1,13 +1,11 @@
 var Splash = React.createClass({
-  mixins : [ReactRouter.History],
-  _onClick : function () {
-    this.history.pushState({}, "front");
-  },
+
   render : function () {
     return (
-      <div className="splash" onClick={this._onClick}>
+      <div className="splash" >
         <Splash1/>
         <Splash2/>
+        <Footer/>
       </div>
     );
   }
@@ -15,13 +13,16 @@ var Splash = React.createClass({
 
 
 var Splash1 = React.createClass({
-
+  mixins : [ReactRouter.History],
+  _onClick : function () {
+    this.history.pushState({}, "front");
+  },
   render : function () {
     return (
-      <div id="splash1" >
+      <div id="splash1" onClick={this._onClick} >
         <div className="hook">
-          <h1>WELCOME BACK</h1>
-          <h2>View, Upload, and Share your Gameday Highlights Now!</h2>
+          <div>WELCOME BACK</div>
+          <div>View, Upload, and Share your Gameday Highlights Now!</div>
 
         </div>
       </div>
@@ -56,6 +57,26 @@ var Splash2 = React.createClass ( {
           <h2> What would you like to do?</h2>
         </div>
         <SplashBar/>
+      </div>
+    )
+  }
+});
+
+var Footer = React.createClass ({
+  _linkedIn : function () {
+    window.location.href="https://www.linkedin.com/in/ahoang512";
+  },
+
+  _github : function () {
+    window.location.href="https://github.com/ahoang512/ballr";
+  },
+  render : function () {
+    return (
+      <div id="footer">
+        <div className="footerContent">
+          <div onClick={this._github}><img src="/github_logo_white.png"/></div>
+          <div onClick={this._linkedIn}><img src="/linked.png"/></div>
+        </div>
       </div>
     )
   }
