fe9386285b9c8217663b86a4c353305d0a8ee2bb
Anthony Hoang <ant.hoang93@gmail.com>
2016-02-23 21:53:20 -0800

    Implement adding comments

--- app/assets/javascripts/components/user_page/comments.js.jsx
+++ app/assets/javascripts/components/user_page/comments.js.jsx
@@ -1,4 +1,5 @@
 var Comments = React.createClass({
+
   getInitialState : function () {
     return ({
     });
@@ -7,26 +8,46 @@ var Comments = React.createClass({
 
 
   render : function () {
-    var addComment = false;
-
-    if (this.state.newComment !== "Add Comment"){
-      addComment = true;
-    }
     return (
       <div className="commentsContainer">
         <Comment/>
-        <NewComment/>
+        <NewComment photo_id={this.props.photo_id}/>
       </div>
     );
   }
 });
 
 var NewComment = React.createClass({
+  getInitialState : function() {
+    return ({
+      text : ""
+    })
+  },
+
+  _handleSubmit : function(e) {
+    e.preventDefault();
+    var params = {
+      user_id : window.current_user,
+      photo_id : this.props.photo_id,
+      text : this.state.text
+    }
+    CommentUtil.createComment(params);
+  },
+
+  _onChange : function(e) {
+    this.setState({
+      text: e.target.value
+    });
+
+  },
+
   render: function () {
     return (
       <div className="newCommentContainer group">
-        <textarea placeholder="Add Comment"/>
-        <button className="commentButton">Comment</button>
+        <form onSubmit={this._handleSubmit}>
+          <textarea placeholder="Add Comment" onChange={this._onChange}/>
+          <button className="commentButton">Comment</button>
+        </form>
       </div>
     )
   }
