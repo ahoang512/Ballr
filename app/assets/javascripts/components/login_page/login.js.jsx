cbe1cd713c96f7a0073cfc2392b633b73599d816
Anthony Hoang <ant.hoang93@gmail.com>
2016-02-28 16:46:11 -0800

    Edit login page

--- app/assets/javascripts/components/login_page/login.js.jsx
+++ app/assets/javascripts/components/login_page/login.js.jsx
@@ -1,3 +1,24 @@
 var Login = React.createClass({
+  render : function (){
+    return (
+      <div class="loginPage">
+        <div class="loginBox">
+          <div class="loginLogo"><img src="/logo2.png"/></div>
+          <h1>Welcome back!</h1>
 
+          <form  action="<%= session_url %>" method="post">
+            <label class="email">Email
+            </label>
+              <input type="text" name="user[email]"/>
+            <label class="password">
+              Password
+            </label>
+            <input type="password" name="user[password]" />
+            <br/>
+            <input type="submit"  id="loginButton" value="Login"/>
+          </form>
+        </div>
+      </div>
+    );
+  }
 });
