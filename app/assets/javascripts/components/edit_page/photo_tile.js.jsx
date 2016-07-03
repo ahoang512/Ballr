f87fc5ce45d16d7d8bdd872612ad3b8f6b84376f
Anthony Hoang <ant.hoang93@gmail.com>
2016-07-03 15:15:21 -0700

    Add hover to play in edit album page

--- app/assets/javascripts/components/edit_page/photo_tile.js.jsx
+++ app/assets/javascripts/components/edit_page/photo_tile.js.jsx
@@ -44,20 +44,46 @@ var PhotoTile = React.createClass({
     PhotoActions.photoClicked(parseInt(e.currentTarget.id));
   },
 
+  _onMouseOver : function() {
+    if (this.state.gif){
+      this.setState({
+        hover : true
+      })
+    }
+  },
+  _onMouseOut : function () {
+    if (this.state.gif) {
+      this.setState({
+        hover : false
+      });
+    }
+  },
+
   render : function () {
+    var url = this.state.frame_url;
+    var className = "photoTiles";
     var selected = "";
     var photo = this.props.photo;
     var photoSelected = this.props.photoSelected;
     if (photoSelected.id === photo.id){
       selected = "selected";
     }
+    if (this.state.gif){
+      className += " gif";
+    }
+    if (this.state.hover){
+      url = this.state.photo_url;
+    }
+
 
     return (
       <li key={photo.id}
           id = {photo.id}
           onClick={this._handleClick}
-          className={selected}>
-          <img src={photo.url}  className="photoTiles"/>
+          className={selected}
+          onMouseOver={this._onMouseOver}
+          onMouseOut= {this._onMouseOut}>
+          <img src={url}  className={className}/>
           <div className="group photoTileInfo">
               <h2>{photo.name}</h2>
           </div>
